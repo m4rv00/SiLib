bcd7c10bd2ab8d35ab6fa2281894c6ad1477f777
Marvin <46823387+m4rv00@users.noreply.github.com>
2024-07-27 22:57:02 +0200

    Commenting

--- src/main/kotlin/SiLib.kt
+++ src/main/kotlin/SiLib.kt
@@ -4,6 +4,17 @@ import kotlin.math.abs
 
 class SiLib {
     companion object {
+
+        /**
+         * Apply SI prefix scaling to any Number type.
+         *
+         * 100.si(Prefix.KILO) -> 100_000
+         * 15.si(Prefix.NANO) -> 0.000_000_015
+         *
+         * @param prefix the SI prefix to apply, defaults to NONE which has a factor of 1.0.
+         * @return the scaled number as a Double.
+         * @throws IllegalArgumentException if the type T is not supported.
+         */
         inline fun <reified T : Number?> T.si(prefix: Prefix = Prefix.NONE): Double {
             return when (this) {
                 is Int -> (this.toDouble() * prefix.factor)
@@ -15,11 +26,21 @@ class SiLib {
             }
         }
 
+        /**
+         * Extension function for Double? to format it as a string with a unit and SI prefix.
+         * 100_000 m -> 100 km
+         * 0.1 s -> 100 ms
+         *
+         * @param unit the physical unit to append to the number after scaling.
+         * @param precision the number of decimal places in output
+         * @return a formatted string representing the number with SI prefix and unit, or an empty string if the number is null.
+         */
         fun Double?.unit(unit: Unit, precision: Int): String {
             if (this == null) {
                 return ""
             }
 
+            // Find fitting prefix based on absolute magnitude of number
             val prefix = Prefix.entries.toTypedArray().findLast { abs(this) >= it.factor } ?: Prefix.NONE
 
             return String.format("%.${precision}f %s%s", this / prefix.factor, prefix.symbol, unit.symbol)
