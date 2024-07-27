fec20ba93490ba363d15459ecf9a5d694dbded62
Marvin <46823387+m4rv00@users.noreply.github.com>
2024-07-27 21:58:20 +0200

    Created function Skeletons

---
+++ src/main/kotlin/SiLib.kt
@@ -0,0 +1,17 @@
+package de.m4rv00
+
+import kotlin.math.abs
+
+class SiLib {
+
+
+    companion object {
+        inline fun <reified T : Number?> T.si(prefix: Prefix = Prefix.NONE): Double {
+
+        }
+
+        fun Double?.unit(unit: Unit, precision: Int): String {
+
+        }
+    }
+}
